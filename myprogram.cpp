e20e5f148b855af73c63597c7eedb0f77ecf9d35
Wenovsky <wenovsky@gmail.com>
2025-05-29 20:23:54 +0200

    Update myprogram.cpp

--- myprogram.cpp
+++ myprogram.cpp
@@ -1,16 +1,18 @@
 /* 	-notepad run: cmd /k cd "c:\work\cpp\Github\learner001\" && gcc cppelso.cpp a.exe
 	-errol a honlaprol kell majd frissitenem a mingw32-t mert a pali azt mondja mashonnan installalt neha nem megyen.: http://www.osdn.net/pfoject/mingw
 	- .cpp file az ugynevezett forraskod file ,  .h az fejlécfile. és a saját header fileokat "idézojelek" !! koze kell rakni , a standardokat <kacsacsor> !
-	- 
+	- mt 733908469
 */
 
 #include <iostream> // ez egy behivott file, elore elkeszitettek a mesterek standard libraryt-abban van. Tartalmaz classokat es ojjektumokat is
 #include <windows.h> // valami nemtommi , nem talaltam a cpp-reference-ben. asszem gui - azaz grafikus library windowsra. de ennek utana köll nézni.
-
 using namespace std;  //a namespace az nemtom mia f.-ene, :: a jele.
 
 
-int heapValtozo;
+int heapValtozo; //?? vagy global??
+void egyikFunkcio(); 
+void masikFunkcio();
+
 
 int main() {// fõ "beépõ" függvény - azaz törzsprogram,azaz a program belepesi pontja. De miért int - who knows...? (oké 0-a jó kimenete,but who cares?) 
 
@@ -23,16 +25,23 @@ int main() {// fõ "beépõ" függvény - azaz törzsprogram,azaz a program belepesi p
 		std::cout << "1) funyiro" << std::endl;
 		std::cout << "2) cicazas" << std::endl;
 		std::cout << "3) hahaha lehetoség" << std::endl;
-		std::cout << "4) valami ide -halom- heapvaltozo:  " << heapValtozo << std::endl;
-		std::cout << "Válassz egy lehetoséget (1-3): ";
+		std::cout << "4) forszarozas" << std::endl;
+		std::cout << "5) tömb változó:  " << std::endl;
+		std::cout << "6) valami ide nemvalasztas -halom- heapvaltozo:  " << heapValtozo << std::endl; //aznemtommiaf@sz
+		std::cout << "Válassz egy lehetoséget (1-6): ";
 		std::cin >> valasztas;
 	
 	
 		switch (valasztas) {
 			case 1: {	
 				std::cout << "Elso lehetoség" << std::endl;
-				std::cout << "funyiro-kapa motorja: briggs quantum xl 55  model type: 128802-0910-01 code: 99022557 briggs quantum xl 55  \n - de te ezzel ne is foglalkozz. lol.";
+				std::cout << "funyiro-kapa motorja: briggs quantum xl 55  model type: 128802-0910-01 code: 99022557 briggs quantum xl 55  \n - de te ezzel ne is foglalkozz. lol." << std::endl;
+				std::cout << "na nezzunk egy kis pointeresdit:\n " << std::endl ;
+				int egyNormalValtozo = 4;
+				int* egyNormalValtozoPointere = &egyNormalValtozo; 
+				std::cout << "itt: " << egyNormalValtozo  << " itt lakik: " << &egyNormalValtozo << " " << " emmeg a pojinter: " << egyNormalValtozoPointere <<std::endl ;
 				std::cout << std::endl;
+				std::cout << "jo\n vagy nem jo?" << std::endl ;
 			break;
 			}
 			
@@ -41,6 +50,8 @@ int main() {// fõ "beépõ" függvény - azaz törzsprogram,azaz a program belepesi p
 				std::cout << "masodik lehetoség:" << std::endl;
 				std::string szovegValtozoNev;
 				std::cout << std::endl;
+				heapValtozo = 10;   // fentiheap kerdesmukodik-e, csakmertnemertem
+				egyikFunkcio();     //
 				std::cout << " Hello,mi a neved te nyomesz? '\n' ";
 				std::cin >> szovegValtozoNev;
 				std::cout << "szia " << szovegValtozoNev << " cica, de jó vagy!";
@@ -68,9 +79,41 @@ int main() {// fõ "beépõ" függvény - azaz törzsprogram,azaz a program belepesi p
 			break;
 			}
 			
+			case 4: {
+				int i;
+				for (i=1 ;  i<11 ; i++ ) {
+					std::cout << "helyi fasz: " << i << endl;
+				}
+				
+				for (heapValtozo=11 ;  heapValtozo<21 ; heapValtozo++ ) {
+					std::cout << "heapfasz: " << heapValtozo << endl;
+				}		
+				
+				/*for (globalValtozo=21 ;  globalValtozo<31 ; globalValtozo++ ) {
+					std::cout << globalValtozo << endl;
+				}
+				*/
+				
+			break;
+			}
+			
+			case 5: {	
+				std::cout << "ötödik lehetoség - még nem csinál semmit,tömb valami." << std::endl;
+				std::cout << "lol.";
+				std::cout << std::endl;
+			break;
+			}
+			
+			
+			
 			default: {
-				std::cout << "Ismeretlen választás. heapvaltozo:  " << heapValtozo << std::endl;
-				std::cout << "a char:" << sizeof(char) << " a short: " << sizeof(short) << " a long: " << sizeof(long) << " és a longlong: " << sizeof(long long) << " byte.";
+				 int weekdaysGametime[7] = {353,35,33,224,224,24,244};  
+				 string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+				 char karakterTomb[5] = "elem";  // vegyukeszre -idezojel! es egy lezáro karakter! ezert 5 elemu. 
+												 // A fordito automatan odateszi: ['e','l','e','m','\0'] - ha nem talál \0 jelet,akkor addig keres a memoriaban,mig nem talal valahol egy '\0' lezarot.
+				std::cout << " !  " << cars[2] << std::endl;
+				std::cout << "Ismeretlen választás. happy new year!  " << heapValtozo << std::endl;
+				
             break;
 			}
 		}
@@ -82,4 +125,12 @@ int main() {// fõ "beépõ" függvény - azaz törzsprogram,azaz a program belepesi p
 	std::cin.get(); // mivel ures mar, ezert uj entert ker
 	
 	return (0);
+}
+
+void egyikFunkcio() {																						//fuggvenyek atrakasa mashova, main utanra
+	cout << "hello heapvaltozo " << heapValtozo << std::endl ;
+}
+
+void masikFunkcio() {
+	cout << "hello masik-heapvaltozo " << heapValtozo << std::endl ;
 }
